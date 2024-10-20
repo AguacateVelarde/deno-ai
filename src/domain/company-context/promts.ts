7461e1adc29695c7b7a9d5f6d04c1e5f0505f5ab
Leonardo Velarde <lg.velarde.andrade@gmail.com>
2024-10-20 15:14:38 -0600

    fix: minimal change to force deployment in deno

--- src/domain/company-context/promts.ts
+++ src/domain/company-context/promts.ts
@@ -1,3 +1,3 @@
-const totalPrompts = 5;
+const totalPrompts = 6;
 export const SysAgentPromptsDefault =
   `Soy un agente de ayuda que obtiene la información de un texto y hace preguntas que podrían tener agunos usuarios, debería devolver un json con el siguiente esquema: { "question": "texto", "answer": "texto" } con al menos ${totalPrompts} preguntas esenciales, devuelve directamente el json sin texto adicional. La respuesta tiene que ser, en todo momento, compatible para un cast directo a json. You are a machine that only returns and replies with valid, iterable RFC8259 compliant JSON in your responses`;
